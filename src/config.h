39033ef05ee09d8d342cf75e4a7ed40b9a6ddfb0
6qyzak <Blasic5763@gmail.com>
2019-05-27 16:57:48 +0900

    Remove unused things

--- src/config.h
+++ src/config.h
@@ -2,8 +2,7 @@
 #define __QYZK_OHNO_CONFIG_H__
 
 #include <filesystem>
-
-#include <boost/asio/ip/tcp.hpp>
+#include <string>
 
 namespace qyzk::ohno
 {
@@ -11,20 +10,13 @@ namespace qyzk::ohno
 class config
 {
 public:
-    using hosts_type = decltype(
-        std::declval< boost::asio::ip::tcp::resolver >().resolve(
-            std::declval< std::string >(),
-            std::declval< std::string >()));
-
     config();
     auto load(std::filesystem::path const& config_path) -> void;
     auto get_token() const noexcept -> std::string const&;
     auto get_api_version() const noexcept -> uint32_t;
     auto get_gateway_version() const noexcept -> uint32_t;
     auto get_discord_url() const noexcept -> std::string const&;
-    auto get_discord_api_url() const noexcept -> std::string const&;
     auto get_discord_hostname() const noexcept -> std::string const&;
-    auto get_discord_hosts() const noexcept -> hosts_type const&;
 
 private:
     std::string m_token;
@@ -33,7 +25,6 @@ private:
     std::string const m_url_discord;
     std::string m_url_discord_api;
     std::string const m_hostname_discord;
-    hosts_type m_hosts_discord;
 }; // class qyzk::ohno::config
 
 } // namespace qyzk
